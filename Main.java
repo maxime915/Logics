e35bbdafa4c0ab619adc1286ec8cbafe433e18a5
maxime915 <maxime.amodei@gmail.com>
2019-06-19 16:21:49 +0200

    Use new interface

--- Main.java
+++ Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
         String s = (args.length > 0 ? args[0] : "!(VALID)");
 
-        LogicChain ln = LogicChain.fromString(s);
-        System.out.println(ln);
-        System.out.println(ln.getTruthTable());
+        LogicFunction lf = new LogicChain().fromString(s);
+        System.out.println(lf);
+        System.out.println(lf.getTruthTableRepresentation());
 
-        System.out.println("min terms: " + Arrays.toString(ln.getMinTermsIndex()));
-        System.out.println(ln.asSumOfMinterms());
-        System.out.println("max terms: " + Arrays.toString(ln.getMaxTermsIndex()));
-        System.out.println(ln.asProdfMaxterms());
+        System.out.println("min terms: " + Arrays.toString(lf.getMinTermsIndex()));
+        System.out.println(lf.expressAsSumOfMinterms());
+        System.out.println("max terms: " + Arrays.toString(lf.getMaxTermsIndex()));
+        System.out.println(lf.expressAsProductOfMaxterms());
     }
 }
