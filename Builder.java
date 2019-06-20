b762e0d0a387a148a70cbd08d6f305c1274137e3
maxime915 <maxime.amodei@gmail.com>
2019-06-20 12:58:14 +0200

    changed approach to solve bugs

--- Builder.java
+++ Builder.java
@@ -1,15 +1,5 @@
 import java.util.Arrays;
-import java.util.Random;
 import java.util.regex.*;
-
-/*
-    remaining bugs for entry : 
-        "A+(B*C)"
-        "(A+B)*C+D"
-        "0+(A+B)*C+D"
-        "(A+B)*C+D"
-        "Z+(A+B)C+D"
-*/
 
 public class Builder {
 
@@ -66,7 +56,7 @@ public class Builder {
         for (int i = 0; i < numberOfVariable; i++)
             nodes[i] = LogicNode.Variable(values, i, names[i]);
 
-        first = buildRec(sequence);
+        first = parse(sequence);
     }
 
     // getters
@@ -87,135 +77,63 @@ public class Builder {
         return query;
     }
 
-    private LogicNode buildRec(char[] sequence) {
-        boolean noBracket = true;
-        for (int i = 0; i < sequence.length && noBracket; i++)
-            if (sequence[i] == '(')
-                noBracket = false;
-
-        if (noBracket)
-            return buildRecBracketLess(sequence);
-        else if (new Random().nextInt(3) > 4) // always true but the compiler doesn't know it
-            throw new RuntimeException("/!\\ rest is not implemented ");
+    private LogicNode parse(char[] sequence) {
+        if (sequence.length < 1)
+            throw new RuntimeException("Empty sequence");
 
         int count = 0;
-        int i0 = -1;
+        // search for OR in max scope
         for (int i = 0; i < sequence.length; i++) {
-            if (sequence[i] == '(') {
-                if (count == 0)
-                    i0 = i;
+            if (sequence[i] == '(')
                 count++;
-            } else if (sequence[i] == ')') {
+            else if (sequence[i] == ')')
                 count--;
+            else if (count == 0 && sequence[i] == '|')
+                return LogicNode.OR(parse(Arrays.copyOfRange(sequence, 0, i - 1 + 1)),
+                        parse(Arrays.copyOfRange(sequence, i + 1, sequence.length)));
 
-                if (count != 0)
-                    continue;
-
-                // end of bracket !
-
-                LogicNode n = buildRec(Arrays.copyOfRange(sequence, i0 + 1, i - 1 + 1));
-
-                if (i0 > 0 && sequence[i0 - 1] == '!') {
-                    n = LogicNode.NOT(n);
-                    i0--;
-                }
-
-                // get first part if &
-                if (i0 > 0 && sequence[i0 - 1] == '&') {
-                    int tempI = i0 - 1;
-                    while (tempI > 0 && sequence[tempI] != '|')
-                        tempI--;
-                    if (tempI == 0) {
-                        n = LogicNode.AND(buildRec(Arrays.copyOfRange(sequence, 0, i0 - 1 - 1 + 1)), n);
-                        // let i0 such that [i0 - 1] == '&'
-                    } else {
-                        LogicNode mid = buildRec(Arrays.copyOfRange(sequence, tempI + 1, i0 - 1 - 1 + 1));
-                        n = LogicNode.AND(mid, n);
-                        i0 = tempI + 1; // position right after |
-                    }
-                } else if (i0 > 0 && sequence[i0 - 1] != '|')
-                    throw new RuntimeException(
-                            "unexpected sequence at position " + (i0 - 1) + " in sequence" + Arrays.toString(sequence));
-
-                if (i + 1 >= sequence.length) {
-                    if (i0 > 0 && sequence[i0 - 1] == '|')
-                        n = LogicNode.OR(buildRec(Arrays.copyOfRange(sequence, 0, i0 - 1 + 1)), n);
-                    return n;
-                }
-
-                if (sequence[i + 1] == '|') {
-                    if (i0 > 0 && sequence[i0 - 1] == '|') {
-                        LogicNode pre = buildRec(Arrays.copyOfRange(sequence, 0, i0 - 1 - 1 + 1));
-                        n = LogicNode.OR(pre, n);
-                    }
-                    return LogicNode.OR(n, buildRec(Arrays.copyOfRange(sequence, i + 2, sequence.length - 1 + 1)));
-                } else if (sequence[i + 1] != '&')
-                    throw new RuntimeException(
-                            "unexpected sequence at position " + (i0 - 1) + " in sequence" + Arrays.toString(sequence));
-
-                // sequence[i+1] == '&'
-                int tempI = i + 1;
-                while (tempI < sequence.length && sequence[tempI] != '|')
-                    tempI++;
-                if (tempI == sequence.length) {
-                    n = LogicNode.AND(n, buildRec(Arrays.copyOfRange(sequence, i + 1 + 1, sequence.length - 1 + 1)));
-                } else {
-                    LogicNode mid = buildRec(Arrays.copyOfRange(sequence, i + 1 + 1, tempI - 1 + 1));
-                    LogicNode post = buildRec(Arrays.copyOfRange(sequence, tempI + 1, sequence.length - 1));
-                    n = LogicNode.OR(LogicNode.AND(n, mid), post);
-                }
-
-                if (i0 > 0 && sequence[i0 - 1] == '|') {
-                    LogicNode pre = buildRec(Arrays.copyOfRange(sequence, 0, i0 - 1 - 1 + 1));
-                    n = LogicNode.OR(pre, n);
-                }
-
-                return n;
-            }
-        }
-        return null;
-    }
-
-    private LogicNode buildRecBracketLess(char[] sequence) {
-        // System.out.println("building for : " + new String(sequence));
-
-        // only : A-Z0-1!|&
-
-        if (sequence.length == 0)
-            throw new RuntimeException("null sequence");
-
-        if (sequence.length == 1) {
-            if (sequence[0] == '0')
-                return LogicNode.FALSE();
-            if (sequence[0] == '1')
-                return LogicNode.TRUE();
-            try {
-                return nodes[indexMapping[(int) sequence[0] - (int) letters[0]]];
-            } catch (IndexOutOfBoundsException e) {
-                e.printStackTrace();
-                throw new RuntimeException("Unrocognized token: " + sequence[0]);
-            }
+            if (count < 0)
+                throw new RuntimeException("Unbalenced ()");
         }
 
-        // find first |
-        for (int i = 0; i < sequence.length; i++)
-            if (sequence[i] == '|')
-                return LogicNode.OR(buildRecBracketLess(Arrays.copyOfRange(sequence, 0, i - 1 + 1)),
-                        buildRecBracketLess(Arrays.copyOfRange(sequence, i + 1, sequence.length - 1 + 1)));
+        if (count != 0)
+            throw new RuntimeException("Unbalenced ()");
 
-        // only & left
-        for (int i = 0; i < sequence.length; i++)
-            if (sequence[i] == '&')
-                return LogicNode.AND(buildRecBracketLess(Arrays.copyOfRange(sequence, 0, i - 1 + 1)),
-                        buildRecBracketLess(Arrays.copyOfRange(sequence, i + 1, sequence.length - 1 + 1)));
+        // search for AND in max scope
+        for (int i = 0; i < sequence.length; i++) {
+            if (sequence[i] == '(')
+                count++;
+            else if (sequence[i] == ')')
+                count--;
+            else if (count == 0 && sequence[i] == '&')
+                return LogicNode.AND(parse(Arrays.copyOfRange(sequence, 0, i - 1 + 1)),
+                        parse(Arrays.copyOfRange(sequence, i + 1, sequence.length)));
+        }
 
-        if (sequence.length != 2)
-            throw new RuntimeException("Houston, we have a probleme up here...");
+        // check for NOT
+        if (sequence[0] == '!')
+            return LogicNode.NOT(parse(Arrays.copyOfRange(sequence, 1, sequence.length)));
 
-        if (sequence[0] != '!')
-            throw new RuntimeException("Houston, we have a probleme up here (2)...");
+        // check for bracket
+        if ((sequence[0] == '(') != (sequence[sequence.length - 1] == ')')) // check for () at outer most pos
+            throw new RuntimeException("not anticipated");
+        if (sequence[0] == '(')
+            return parse(Arrays.copyOfRange(sequence, 1, sequence.length - 1));
 
-        return LogicNode.NOT(buildRecBracketLess(Arrays.copyOfRange(sequence, 1, 2)));
+        // should only have a signle variable now (A-Z0-1)
+        if (sequence.length != 1)
+            throw new RuntimeException("didn't expect that...");
+
+        if (sequence[0] == '0')
+            return LogicNode.FALSE();
+        if (sequence[0] == '1')
+            return LogicNode.TRUE();
+        try {
+            return nodes[indexMapping[(int) sequence[0] - (int) letters[0]]];
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Unrocognized token: " + sequence[0]);
+        }
     }
 
     private static String clean(String s) {
