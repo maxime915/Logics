d71fd22a944b1a8242974f2741fb24a7779046da
maxime915 <maxime.amodei@gmail.com>
2019-09-18 15:38:49 +0200

    prompt user for logic expression

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String s = (args.length > 0 ? args[0] : "!(VALID)");
-
+    public static void doSomething(String s) {
         LogicFunction lf = new LogicChain(s);
         System.out.println(lf);
         System.out.println(lf.getTruthTableRepresentation());
@@ -13,5 +12,24 @@ public class Main {
         System.out.println(lf.expressAsSumOfMinterms());
         System.out.println("max terms: " + Arrays.toString(lf.getMaxTermsIndex()));
         System.out.println(lf.expressAsProductOfMaxterms());
+    }
+
+    public static void main(String[] args) {
+        if (args.length > 0)
+            doSomething(args[0]);
+
+        String s;
+
+        Scanner scan = new Scanner(System.in);
+
+        for (;;) {
+            System.out.print("Enter a new boolean expression: ");
+            s = scan.nextLine();
+            if (s.equals("no"))
+                break;
+            doSomething(s);
+        }
+
+        scan.close();
     }
 }
